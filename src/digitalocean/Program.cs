71d5fe42aeba4de2203753a19d3371583982594b
Aaron Stannard <aaron@petabridge.com>
2024-12-07 11:33:18 -0600

    have compilation working, we think

--- src/digitalocean/Program.cs
+++ src/digitalocean/Program.cs
@@ -1,37 +1,49 @@
 using Pulumi;
-using Pulumi.Digitalocean;
 using System.Collections.Generic;
+using Pulumi.DigitalOcean;
+using Config = Pulumi.Config;
 
 return await Deployment.RunAsync(() =>
 {
     // Define configuration options
     var config = new Config();
-    var dropletSize = config.Get("dropletSize") ?? "s-1vcpu-1gb";
-    var region = config.Get("region") ?? "nyc1"; // default to New York data center
+    var dropletSize = config.Require("dropletSize");
+    var region = config.Require("dropletRegion");
+    var image = config.Require("dropletImage");
 
     // Retrieve the Tailscale auth key from Pulumi secrets
     var tailscaleAuthKey = config.RequireSecret("tailscaleAuthKey");
 
     // Define cloud-init script to join Tailscale with the provided auth key
-    var cloudInitScript = @"
-        #cloud-config
-        runcmd:
-          - curl -fsSL https://tailscale.com/install.sh | sh
-          - tailscale up --authkey " + tailscaleAuthKey.Apply(key => key) + @"
-    ";
+    var cloudInitScript = $"""
+                             
+                             #cloud-config
+                             runcmd:
+                               - curl -fsSL https://tailscale.com/install.sh | sh
+                               - tailscale up --authkey {tailscaleAuthKey.Apply(key => key)} --accept-routes --ssh --advertise-exit-node
+                           """;
+    
+    // Create a DigitalOcean VPC
+    var vpc = new Vpc("my-vpc", new VpcArgs
+    {
+        Region = region,
+        IpRange = "10.10.10.0/24", // make sure this doesn't overlap with other networks on your account
+    });
 
     // Create a new Droplet instance
-    var droplet = new Droplet("my-droplet", new DropletArgs
+    var droplet = new Droplet("tailscale-droplet", new DropletArgs
     {
+        Name = "tailscale-droplet",
         Size = dropletSize,
         Region = region,
-        Image = "ubuntu-20-04-x64",
+        Image = image,
+        VpcUuid = vpc.Id,
         UserData = cloudInitScript // Add the cloud-init script here
     });
 
-    // Export Droplet's Public IP
+    // Export Droplet's IP
     return new Dictionary<string, object?>
     {
-        ["dropletPublicIp"] = droplet.Ipv4Address
+        ["dropletIp"] = droplet.Ipv4Address
     };
 });
